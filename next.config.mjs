c3a9f3e670a286dfacf3cffeddf52574533e5a21
ckhsa03 <ckhsa03@gmail.com>
2024-11-28 21:17:59 +0900

    modified: next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*", // 프론트엔드에서 호출하는 경로
+        destination: "http://192.168.161.24:8080/api/:path*", // 백엔드 실제 주소 (HTTP)
+      },
+    ];
+  },
+};
 
 export default nextConfig;
